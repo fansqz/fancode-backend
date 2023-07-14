7c86e0ba71847397661a1eb6b7b40db93a571e44
fzw-code <81961566+fzw-code@users.noreply.github.com>
2023-07-14 15:56:48 +0800

    添加main启动，绑定模型

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package FanCode
 
 import (
 	"FanCode/db"
+	"FanCode/models"
 	"FanCode/setting"
 	"fmt"
 	"os"
@@ -13,13 +14,20 @@ func main() {
 	path, _ := os.Getwd()
 	path = strings.ReplaceAll(path, "\\", "/")
 	path = path + "/conf/config.ini"
+
 	//加载配置
 	if err := setting.Init(path); err != nil {
 		fmt.Println("加载配置文件出错")
 		return
 	}
+
 	//连接数据库
 	if err := db.InitMysql(setting.Conf.MySqlConfig); err != nil {
 		fmt.Println("数据库连接失败")
 	}
+
+	// 模型绑定
+	db.DB.AutoMigrate(&models.User{})
+	defer db.CloseMysql()
+
 }
