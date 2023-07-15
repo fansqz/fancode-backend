25fd707afa5392e9408d61c87bbbc4f945a3b6e0
fzw-code <81961566+fzw-code@users.noreply.github.com>
2023-07-15 12:05:08 +0800

    ”âadd cos

--- store/cos.go
+++ store/cos.go
@@ -8,6 +8,7 @@ import (
 	"github.com/tencentyun/cos-go-sdk-v5/debug"
 	"net/http"
 	"net/url"
+	"os"
 	"strings"
 )
 
@@ -20,7 +21,7 @@ func NewCOS() *COS {
 	u, _ := url.Parse(fmt.Sprintf("http://%s-%s.cos.%s.myqcloud.com",
 		storeConfig.BucketName, storeConfig.AppID, storeConfig.Region))
 	b := &cos.BaseURL{BucketURL: u}
-	c := cos.NewClient(b, &http.Client{
+	client := cos.NewClient(b, &http.Client{
 		Transport: &cos.AuthorizationTransport{
 			SecretID:  storeConfig.SecretID,
 			SecretKey: storeConfig.SecretKey,
@@ -33,17 +34,50 @@ func NewCOS() *COS {
 		},
 	})
 	return &COS{
-		client: c,
+		client: client,
 	}
 }
 
-func (cos *COS) SaveFile(storePath string, file *strings.Reader) error {
-	_, err := cos.client.Object.Put(context.Background(), storePath, file, nil)
+func (c *COS) SaveFile(storePath string, file *strings.Reader) error {
+	_, err := c.client.Object.Put(context.Background(), storePath, file, nil)
 	return err
 }
 
 // storePaht:对象存储的路劲，loadPath:本地路径
-func (cos *COS) LoadFile(storePath, localPath string) error {
-	_, err := cos.client.Object.GetToFile(context.Background(), storePath, localPath, nil)
+func (c *COS) LoadFile(storePath, localPath string) error {
+	_, err := c.client.Object.GetToFile(context.Background(), storePath, localPath, nil)
 	return err
+}
+
+func (c *COS) LoadFolder(storePath, localPath string) error {
+	// 列出文件夹下的对象
+	objects, _, err := c.client.Bucket.Get(context.TODO(), &cos.BucketGetOptions{
+		Prefix:    storePath,
+		Delimiter: "/",
+	})
+	if err != nil {
+		fmt.Println("Error:", err)
+		return err
+	}
+
+	for _, obj := range objects.Contents {
+		// 获取文件的名称
+		filename := obj.Key[strings.LastIndex(obj.Key, "/")+1:]
+		// 创建本地文件
+		file, err := os.Create(filename)
+		if err != nil {
+			fmt.Println("Error:", err)
+			return err
+		}
+		defer file.Close()
+
+		// 下载文件
+		_, err = c.client.Object.Get(context.TODO(), obj.Key, nil)
+		if err != nil {
+			fmt.Println("Error:", err)
+		} else {
+			fmt.Printf("Downloaded %s\n", filename)
+		}
+	}
+	return nil
 }
