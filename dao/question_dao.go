8c3fc6e83cd3353b383c8c1919968a0cb6d1b9c4
fzw-code <81961566+fzw-code@users.noreply.github.com>
2023-07-15 23:08:06 +0800

    add updatePathByNumber

--- dao/question_dao.go
+++ dao/question_dao.go
@@ -38,8 +38,8 @@ func GetQuestionByQuestionID(questionID uint) (*models.Question, error) {
 func UpdateQuestion(question *models.Question) error {
 	sqlStr := "update `questions` set name = ?, number = ?, discriptioin = ?, title = ?, path = ? where id = ?"
 	//执行
-	db.DB.Exec(sqlStr, question.Name, question.Number, question.Description, question.Title, question.Path, question.ID)
-	return nil
+	err := db.DB.Exec(sqlStr, question.Name, question.Number, question.Description, question.Title, question.Path, question.ID).Error
+	return err
 }
 
 // CheckUserID检测用户ID是否存在
@@ -51,9 +51,16 @@ func CheckQuestionNumber(questionNumber string) bool {
 	return question.Number != ""
 }
 
-func GetQuestionList(page int, pageSize int) []*models.Question {
+func GetQuestionList(page int, pageSize int) ([]*models.Question, error) {
 	offset := (page - 1) * pageSize
 	var questions []*models.Question
-	db.DB.Limit(pageSize).Offset(offset).Find(questions)
-	return questions
+	err := db.DB.Limit(pageSize).Offset(offset).Find(questions).Error
+	return questions, err
+}
+
+func UpdatePathByNumber(path string, questionNumber string) error {
+	sqlStr := "update `questions` set path = ? where number = ?"
+	//执行
+	err := db.DB.Exec(sqlStr, path, questionNumber).Error
+	return err
 }
