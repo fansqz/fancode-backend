43218cdac61fc0e18302677f967c31a6a4a758c0
fzw-code <81961566+fzw-code@users.noreply.github.com>
2023-07-25 11:11:53 +0800

    style: 简单修改下命名

--- controllers/problem_handler.go
+++ controllers/problem_handler.go
@@ -21,25 +21,25 @@ type ProblemController interface {
 }
 
 type problemController struct {
-	ProblemService service.ProblemService
+	problemService service.ProblemService
 }
 
 func NewProblemController() ProblemController {
 	return &problemController{
-		ProblemService: service.NewProblemService(),
+		problemService: service.NewProblemService(),
 	}
 }
 
 func (q *problemController) InsertProblem(ctx *gin.Context) {
 	result := r.NewResult(ctx)
-	Problem := &po.Problem{}
-	Problem.Number = ctx.PostForm("number")
-	Problem.Name = ctx.PostForm("name")
-	Problem.Description = ctx.PostForm("description")
-	Problem.Title = ctx.PostForm("title")
-	Problem.Path = ctx.PostForm("path")
+	problem := &po.Problem{}
+	problem.Number = ctx.PostForm("number")
+	problem.Name = ctx.PostForm("name")
+	problem.Description = ctx.PostForm("description")
+	problem.Title = ctx.PostForm("title")
+	problem.Path = ctx.PostForm("path")
 	//插入
-	pID, err := q.ProblemService.InsertProblem(Problem)
+	pID, err := q.problemService.InsertProblem(problem)
 	if err != nil {
 		result.Error(err)
 		return
@@ -49,21 +49,21 @@ func (q *problemController) InsertProblem(ctx *gin.Context) {
 
 func (q *problemController) UpdateProblem(ctx *gin.Context) {
 	result := r.NewResult(ctx)
-	ProblemIDString := ctx.PostForm("id")
-	quesetionID, err := strconv.Atoi(ProblemIDString)
+	problemIDString := ctx.PostForm("id")
+	problemID, err := strconv.Atoi(problemIDString)
 	if err != nil {
 		result.Error(e.ErrBadRequest)
 		return
 	}
 	problem := &po.Problem{}
-	problem.ID = uint(quesetionID)
+	problem.ID = uint(problemID)
 	problem.Number = ctx.PostForm("number")
 	problem.Name = ctx.PostForm("name")
 	problem.Description = ctx.PostForm("description")
 	problem.Title = ctx.PostForm("title")
 	problem.Path = ctx.PostForm("path")
 
-	err2 := q.ProblemService.UpdateProblem(problem)
+	err2 := q.problemService.UpdateProblem(problem)
 	if err != nil {
 		result.Error(err2)
 		return
@@ -79,7 +79,7 @@ func (q *problemController) DeleteProblem(ctx *gin.Context) {
 		result.Error(e.ErrBadRequest)
 		return
 	}
-	err2 := q.ProblemService.DeleteProblem(uint(id))
+	err2 := q.problemService.DeleteProblem(uint(id))
 	if err2 != nil {
 		result.Error(err2)
 		return
@@ -105,7 +105,7 @@ func (q *problemController) GetProblemList(ctx *gin.Context) {
 		result.Error(e.ErrBadRequest)
 		return
 	}
-	pageInfo, err := q.ProblemService.GetProblemList(page, pageSize)
+	pageInfo, err := q.problemService.GetProblemList(page, pageSize)
 	if err != nil {
 		result.Error(err)
 		return
@@ -121,7 +121,7 @@ func (q *problemController) GetProblemByID(ctx *gin.Context) {
 		result.Error(e.ErrBadRequest)
 		return
 	}
-	problem, err2 := q.ProblemService.GetProblemByID(uint(id))
+	problem, err2 := q.problemService.GetProblemByID(uint(id))
 	if err2 != nil {
 		result.Error(err2)
 		return
@@ -136,9 +136,9 @@ func (q *problemController) UploadProblemFile(ctx *gin.Context) {
 		result.Error(e.ErrBadRequest)
 		return
 	}
-	ProblemNumber := ctx.PostForm("problemNumber")
+	problemNumber := ctx.PostForm("problemNumber")
 	// 保存文件到本地
-	uploadErr := q.ProblemService.UploadProblemFile(ctx, file, ProblemNumber)
+	uploadErr := q.problemService.UploadProblemFile(ctx, file, problemNumber)
 	if uploadErr != nil {
 		result.Error(uploadErr)
 		return
