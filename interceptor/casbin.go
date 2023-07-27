455e852b08b23191220d05af6d058918e304e10d
fzw-code <81961566+fzw-code@users.noreply.github.com>
2023-07-27 22:12:36 +0800

    feat:添加casbin配置

--- interceptor/casbin.go
+++ interceptor/casbin.go
@@ -1,6 +1,19 @@
 package interceptor
 
-// 用作访问控制管理的
-func SetUp() {
-	
+import (
+	"github.com/casbin/casbin/v2"
+	gormadapter "github.com/casbin/gorm-adapter/v3"
+	_ "github.com/go-sql-driver/mysql"
+	"log"
+)
+
+func Casbin() *casbin.Enforcer {
+	a, _ := gormadapter.NewAdapter("mysql", "root:miconvert*.*@tcp(49.234.56:3306)/") // Your driver and data source.
+	e, err := casbin.NewEnforcer("./model.conf", a)
+	if err != nil {
+		log.Fatal("载入casbin配置出错")
+	}
+
+	e.LoadPolicy() // 从数据库载入配置
+	return e
 }
