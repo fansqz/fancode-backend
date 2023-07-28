1309456d4e9823e51df5309402d2fb84c05b40ea
fzw-code <81961566+fzw-code@users.noreply.github.com>
2023-07-28 21:29:22 +0800

    feat: 添加api和role的dao方法

--- dao/sys_api_dao.go
+++ dao/sys_api_dao.go
@@ -5,6 +5,11 @@ import (
 	"github.com/jinzhu/gorm"
 )
 
+// CreateApi 创建角色
+func CreateApi(db *gorm.DB, api *po.SysApi) error {
+	return db.Create(api).Error
+}
+
 // GetApiByID 通过api的id获取api
 func GetApiByID(db *gorm.DB, id uint) (*po.SysApi, error) {
 	var api po.SysApi
@@ -20,6 +25,13 @@ func GetSysApiListByParentID(db *gorm.DB, parentID int32) ([]*po.SysApi, error) 
 		return nil, err
 	}
 	return sysApis, nil
+}
+
+// GetApiCount 获取api总数
+func GetApiCount(db *gorm.DB) (uint, error) {
+	var count uint
+	err := db.Model(&po.SysApi{}).Count(&count).Error
+	return count, err
 }
 
 // GetSysApiListByPathKeyword 模糊查询api
--- dao/sys_role_dao.go
+++ dao/sys_role_dao.go
@@ -1,1 +1,46 @@
 package dao
+
+import (
+	"FanCode/models/po"
+	"github.com/jinzhu/gorm"
+)
+
+// CreateRole 创建角色
+func CreateRole(db *gorm.DB, role *po.SysRole) error {
+	return db.Create(role).Error
+}
+
+// UpdateRole 更新角色
+func UpdateRole(db *gorm.DB, role *po.SysRole) error {
+	return db.Save(role).Error
+}
+
+// DeleteRole 删除角色
+func DeleteRole(db *gorm.DB, role *po.SysRole) error {
+	return db.Delete(role).Error
+}
+
+// GetRoleByID 通过角色id获取角色
+func GetRoleByID(db *gorm.DB, roleID uint) (*po.SysRole, error) {
+	var role po.SysRole
+	err := db.First(&role, roleID).Error
+	if err != nil {
+		return nil, err
+	}
+	return &role, nil
+}
+
+// GetRoleList 获取角色列表
+func GetRoleList(db *gorm.DB, page int, pageSize int) ([]*po.SysRole, error) {
+	offset := (page - 1) * pageSize
+	var roles []*po.SysRole
+	err := db.Limit(pageSize).Offset(offset).Find(&roles).Error
+	return roles, err
+}
+
+// GetRoleCount 获取所有角色数量
+func GetRoleCount(db *gorm.DB) (uint, error) {
+	var count uint
+	err := db.Model(&po.SysRole{}).Count(&count).Error
+	return count, err
+}
