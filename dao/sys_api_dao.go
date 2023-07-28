387151983fe377176ec59007ec3db220d1f5c8a9
fzw-code <81961566+fzw-code@users.noreply.github.com>
2023-07-28 21:18:55 +0800

    feat: 添加api相关dao

---
+++ dao/sys_api_dao.go
@@ -0,0 +1,52 @@
+package dao
+
+import (
+	"FanCode/models/po"
+	"github.com/jinzhu/gorm"
+)
+
+// GetApiByID 通过api的id获取api
+func GetApiByID(db *gorm.DB, id uint) (*po.SysApi, error) {
+	var api po.SysApi
+	err := db.First(&api, id).Error
+	return &api, err
+}
+
+// GetSysApiListByParentID 通过父id找到其所有子api
+func GetSysApiListByParentID(db *gorm.DB, parentID int32) ([]*po.SysApi, error) {
+	var sysApis []*po.SysApi
+	err := db.Where("parent_api_id = ?", parentID).Find(&sysApis).Error
+	if err != nil {
+		return nil, err
+	}
+	return sysApis, nil
+}
+
+// GetSysApiListByPathKeyword 模糊查询api
+func GetSysApiListByPathKeyword(db *gorm.DB, keyword string, page int, pageSize int) ([]*po.SysApi, error) {
+	var sysApis []*po.SysApi
+	err := db.Where("path LIKE ?", "%"+keyword+"%").Offset((page - 1) * pageSize).Limit(pageSize).Find(&sysApis).Error
+	if err != nil {
+		return nil, err
+	}
+	return sysApis, nil
+}
+
+// DeleteApiByID 根据api的id进行删除
+func DeleteApiByID(db *gorm.DB, id uint) error {
+	var api []*po.SysApi
+	err := db.First(&api, id).Error
+	if err != nil {
+		return err
+	}
+	err = db.Delete(&api).Error
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+// UpdateApiByID 修改api
+func UpdateApiByID(db *gorm.DB, api *po.SysApi) error {
+	return db.Model(&po.SysApi{}).Where("id = ?", api.ID).Update(api).Error
+}
