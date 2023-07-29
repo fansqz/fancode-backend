e0c2789842038d468f12b122dbd78270658cffe5
fzw-code <81961566+fzw-code@users.noreply.github.com>
2023-07-29 23:40:19 +0800

    feat: 添加role的dto

---
+++ models/dto/sys_role.go
@@ -0,0 +1,24 @@
+package dto
+
+import (
+	"FanCode/models/po"
+	"FanCode/utils"
+)
+
+// SysRoleDtoForList 获取角色列表
+type SysRoleDtoForList struct {
+	ID          uint       `json:"id"`
+	UpdatedAt   utils.Time `json:"updatedAt"`
+	Name        string     `json:"name"`
+	Description string     `json:"description"`
+}
+
+func NewRoleDtoForList(role *po.SysRole) *SysRoleDtoForList {
+	response := &SysRoleDtoForList{
+		ID:          role.ID,
+		UpdatedAt:   utils.Time(role.UpdatedAt),
+		Name:        role.Name,
+		Description: role.Description,
+	}
+	return response
+}
