1b1d8283eee29554ffd7978d45d131119f975cc1
fzw-code <81961566+fzw-code@users.noreply.github.com>
2023-07-30 23:48:09 +0800

    fix: 去除user的enable字段

--- models/po/sys_user.go
+++ models/po/sys_user.go
@@ -13,6 +13,5 @@ type SysUser struct {
 	Phone     string    `gorm:"column:phone" json:"phone"`
 	Avatar    string    `gorm:"column:avatar" json:"avatar"`
 	Remark    string    `gorm:"column:remark" json:"remark"`
-	Enable    bool      `gorm:"column:enable" json:"enable"`
 	Roles     []SysRole `gorm:"many2many:user_roles;" json:"roles"`
 }
