e3a60dc8a71f4ffed5acddc146acc4a387b8eb24
fzw-code <81961566+fzw-code@users.noreply.github.com>
2023-07-30 22:51:15 +0800

    feat: 添加用户管理可能的dao方法

--- dao/sys_user_dao.go
+++ dao/sys_user_dao.go
@@ -11,9 +11,9 @@ func InsertUser(db *gorm.DB, user *po.SysUser) error {
 	return db.Create(user).Error
 }
 
-// UpdateUser 更新用户
+// UpdateUser 更新用户，注：不更新密码
 func UpdateUser(db *gorm.DB, user *po.SysUser) error {
-	return db.Save(user).Error
+	return db.Model(user).Omit("password").Updates(user).Error
 }
 
 // DeleteUserByID 删除用户
@@ -32,10 +32,10 @@ func GetUserByID(db *gorm.DB, id uint) (*po.SysUser, error) {
 }
 
 // GetUserList 获取用户列表
-func GetUserList(db *gorm.DB, page int, pageSize int) ([]*po.SysUser, error) {
+func GetUserList(db *gorm.DB, username string, page int, pageSize int) ([]*po.SysUser, error) {
 	offset := (page - 1) * pageSize
 	var users []*po.SysUser
-	err := db.Limit(pageSize).Offset(offset).Find(&users).Error
+	err := db.Where("username LIKE ?", "%"+username+"%").Limit(pageSize).Offset(offset).Find(&users).Error
 	return users, err
 }
 
@@ -67,4 +67,50 @@ func CheckLoginName(db *gorm.DB, loginname string) (bool, error) {
 		return false, err
 	}
 	return true, nil
+}
+
+// DeleteUserRoleByUserID 清除所有与userID关联的userID-roleID数据
+func DeleteUserRoleByUserID(db *gorm.DB, userID uint) error {
+	user := po.SysUser{}
+	user.ID = userID
+	if err := db.Model(&user).Association("Roles").Clear().Error; err != nil {
+		return err
+	}
+	return nil
+}
+
+// GetRoleIDsByUserID 获取用户关联的所有role的id
+func GetRoleIDsByUserID(db *gorm.DB, userID uint) ([]uint, error) {
+	user := po.SysUser{}
+	user.ID = userID
+	if err := db.Model(&user).Association("Roles").Find(&user.Roles).Error; err != nil {
+		return nil, err
+	}
+	roleIDs := make([]uint, len(user.Roles))
+	for i, role := range user.Roles {
+		roleIDs[i] = role.ID
+	}
+	return roleIDs, nil
+}
+
+// InsertRolesToUser 给用户设置角色
+func InsertRolesToUser(db *gorm.DB, userID uint, roleIDs []uint) error {
+	user := &po.SysUser{}
+	user.ID = userID
+	for _, apiID := range roleIDs {
+		api := &po.SysApi{}
+		api.ID = apiID
+		err := db.Model(user).Association("Roles").Append(api).Error
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+// UpdateUserPassword 更新用户密码
+func UpdateUserPassword(db *gorm.DB, userID uint, password string) error {
+	user := po.SysUser{}
+	user.ID = userID
+	return db.Model(&user).Update("password", password).Error
 }
