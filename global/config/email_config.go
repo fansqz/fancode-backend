2eac26e46165eda7b52a9962d3b323890059d5ec
fzw-code <81961566+fzw-code@users.noreply.github.com>
2023-08-29 15:47:28 +0800

    fix: 修复email配置出错

--- global/config/email_config.go
+++ global/config/email_config.go
@@ -4,9 +4,9 @@ import "gopkg.in/ini.v1"
 
 type EmailConfig struct {
 	Host     string `ini:"host"`
-	Port     int    `int:"port"`
+	Port     int    `ini:"port"`
 	User     string `ini:"user"`
-	Password string `ini:"password"`
+	Password string `ini:"pass"`
 }
 
 func NewEmailConfig(cfg *ini.File) *EmailConfig {
