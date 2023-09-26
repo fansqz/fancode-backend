0fb2a08579567d7b9b28ded39f51b36106b9f91b
fzw <2958556459@qq.com>
2023-09-26 20:44:44 +0800

    feat(bank): 添加题库管理的handler

--- controllers/admin/problem_bank_handler.go
+++ controllers/admin/problem_bank_handler.go
@@ -1,8 +1,13 @@
 package admin
 
 import (
+	"FanCode/controllers"
+	e "FanCode/error"
+	"FanCode/models/po"
+	r "FanCode/models/vo"
 	"FanCode/service"
 	"github.com/gin-gonic/gin"
+	"strconv"
 )
 
 // ProblemBankManagementController
@@ -21,5 +26,112 @@ type ProblemBankManagementController interface {
 }
 
 type problemBankManagementController struct {
-	service.SysApiService
+	problemBankService service.ProblemBankService
+}
+
+func NewProblemBankController() ProblemBankManagementController {
+	return &problemBankManagementController{
+		problemBankService: service.NewProblemBankService(),
+	}
+}
+
+func (p *problemBankManagementController) InsertProblemBank(ctx *gin.Context) {
+	result := r.NewResult(ctx)
+	bank := p.getBank(ctx)
+	pID, err := p.problemBankService.InsertProblemBank(bank, ctx)
+	if err != nil {
+		result.Error(err)
+		return
+	}
+	result.Success("题库添加成功", pID)
+}
+
+func (p *problemBankManagementController) UpdateProblemBank(ctx *gin.Context) {
+	result := r.NewResult(ctx)
+	bank := p.getBank(ctx)
+	// 读取id
+	bankIDString := ctx.PostForm("id")
+	bankID, err := strconv.Atoi(bankIDString)
+	if err != nil {
+		result.Error(e.ErrBadRequest)
+		return
+	}
+	bank.ID = uint(bankID)
+	// 读取文件
+	err2 := p.problemBankService.UpdateProblemBank(bank)
+	if err2 != nil {
+		result.Error(err2)
+		return
+	}
+	result.SuccessData("题库修改成功")
+}
+
+func (p *problemBankManagementController) DeleteProblemBank(ctx *gin.Context) {
+	result := r.NewResult(ctx)
+	// 读取id
+	bankIDString := ctx.PostForm("id")
+	bankID, err := strconv.Atoi(bankIDString)
+	if err != nil {
+		result.Error(e.ErrBadRequest)
+		return
+	}
+	// 判断是否强制删除
+	forceDeleteStr := ctx.PostForm("forceDelete")
+	forceDelete := false
+	if forceDeleteStr == "true" {
+		forceDelete = true
+	}
+	// 删除题库
+	err2 := p.problemBankService.DeleteProblemBank(uint(bankID), forceDelete)
+	if err2 != nil {
+		result.Error(err2)
+		return
+	}
+	result.SuccessData("题库删除成功")
+}
+
+func (p *problemBankManagementController) GetProblemBankList(ctx *gin.Context) {
+	result := r.NewResult(ctx)
+	pageQuery, err := controllers.GetPageQueryByQuery(ctx)
+	if err != nil {
+		result.Error(err)
+		return
+	}
+	// 读取名称和描述
+	bank := &po.ProblemBank{
+		Name:        ctx.PostForm("name"),
+		Description: ctx.PostForm("description"),
+	}
+	pageQuery.Query = bank
+	pageInfo, err := p.problemBankService.GetProblemBankList(pageQuery)
+	if err != nil {
+		result.Error(err)
+		return
+	}
+	result.SuccessData(pageInfo)
+}
+
+func (p *problemBankManagementController) GetProblemBankByID(ctx *gin.Context) {
+	result := r.NewResult(ctx)
+	// 读取id
+	bankIDString := ctx.PostForm("id")
+	bankID, err := strconv.Atoi(bankIDString)
+	if err != nil {
+		result.Error(e.ErrBadRequest)
+		return
+	}
+	bank, err2 := p.problemBankService.GetProblemBankByID(uint(bankID))
+	if err2 != nil {
+		result.Error(err2)
+		return
+	}
+	result.SuccessData(bank)
+}
+
+func (p *problemBankManagementController) getBank(ctx *gin.Context) *po.ProblemBank {
+	bank := &po.ProblemBank{}
+	bank.Name = ctx.PostForm("name")
+	bank.Description = ctx.PostForm("description")
+	bank.Icon = ctx.PostForm("icon")
+	return bank
 }
