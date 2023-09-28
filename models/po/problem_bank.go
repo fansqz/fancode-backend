1521a41794807392f0ff8c3691d9631b37bc0840
fzw <2958556459@qq.com>
2023-09-29 01:00:05 +0800

    feat: 设置题库数据返回的json为驼峰

--- models/po/problem_bank.go
+++ models/po/problem_bank.go
@@ -5,9 +5,9 @@ import "gorm.io/gorm"
 // ProblemBank  题库
 type ProblemBank struct {
 	gorm.Model
-	Name        string    `gorm:"column:name"`
-	Icon        string    `gorm:"column:icon"`
-	Description string    `gorm:"column:description"`
-	CreatorID   uint      `gorm:"column:creator_id"`
-	Problems    []Problem `gorm:"foreignKey:bank_id"`
+	Name        string    `gorm:"column:name" json:"name"`
+	Icon        string    `gorm:"column:icon" json:"icon"`
+	Description string    `gorm:"column:description" json:"description"`
+	CreatorID   uint      `gorm:"column:creator_id" json:"creatorID"`
+	Problems    []Problem `gorm:"foreignKey:bank_id" json:"problems"`
 }
