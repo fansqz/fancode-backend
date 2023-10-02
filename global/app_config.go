4d4b56134796b16287d4b5ef32d62379de8f8b25
fzw <2958556459@qq.com>
2023-10-02 18:32:06 +0800

    fix: 去除放行接口的配置

--- global/app_config.go
+++ global/app_config.go
@@ -3,11 +3,10 @@ package global
 // AppConfig
 // @Description:应用配置
 type AppConfig struct {
-	Release          bool   `ini:"release"` //是否是上线模式
-	Port             string `ini:"port"`    //端口
-	ReleaseStartPath string `ini:"releaseStartPath"`
-	ProUrl           string `ini:"proUrl"`
-	DefaultPassword  string `ini:"defaultPassword"`
+	Release         bool   `ini:"release"` //是否是上线模式
+	Port            string `ini:"port"`    //端口
+	ProUrl          string `ini:"proUrl"`
+	DefaultPassword string `ini:"defaultPassword"`
 	*MySqlConfig
 	*RedisConfig
 	*EmailConfig
